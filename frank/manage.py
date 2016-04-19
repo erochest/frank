c366dfc7af3600357779855c541318bc5422c77a
Eric Rochester <erochest@virginia.edu>
2016-04-19 11:42:35 -0400

    Removed the Makefile in favor of flask-script.

---
+++ frank/manage.py
@@ -0,0 +1,57 @@
+
+"""The manager app."""
+
+import subprocess
+
+from flask.ext.script import Manager
+from flask_migrate import MigrateCommand
+
+from frank.wsgi import app
+
+
+HEROKU_APP = 'frankensystem'
+
+manager = Manager(app)
+
+
+@manager.command
+def freeze():
+    """Freeze the current dependencies to 'requirements.txt'"""
+    proc = subprocess.run(['pip', 'freeze'], stdout=subprocess.PIPE)
+    with open('requirements.txt', 'wb') as fout:
+        fout.write(proc.stdout)
+
+
+@manager.command
+def deploy(branch=None, to='master', keep=False, heroku_app=HEROKU_APP):
+    """Close the current or given branch, merge it, and push it.
+    Then run migrations on the server.
+    """
+    if branch is None:
+        proc = subprocess.run(['git', 'branch'], stdout=subprocess.PIPE)
+        lines = [
+            line[2:]
+            for line in proc.stdout.decode('utf8').splitlines()
+            if line.startswith('* ')
+        ]
+        branch = lines[0]
+
+    assert branch != to
+
+    subprocess.run(['git', 'checkout', to])
+    subprocess.run(['git', 'merge', branch])
+    if not keep:
+        subprocess.run(['git', 'branch', '--delete', branch])
+    subprocess.run(['git', 'push'])
+    subprocess.run([
+        'heroku', 'run', '--app', heroku_app, '--',
+        'python', '-m', 'frank.manage', 'db', 'upgrade',
+    ])
+    subprocess.run(['heroku', 'restart', '--app', heroku_app])
+
+
+manager.add_command('db', MigrateCommand)
+
+
+if __name__ == '__main__':
+    manager.run()
--- frank/wsgi.py
+++ frank/wsgi.py
@@ -5,8 +5,7 @@
 
 from flask import Flask
 from flask.ext.heroku import Heroku
-from flask.ext.script import Manager
-from flask_migrate import Migrate, MigrateCommand
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
 
@@ -20,16 +19,9 @@ heroku = Heroku(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-manager = Manager(app)
-manager.add_command('db', MigrateCommand)
-
 
 @app.route('/')
 def homepage():
     """The root page."""
     n = list(db.session.execute(text('SELECT RANDOM();'), {}))[0][0]
     return "It's alive!\n" + str(n) + "\n"
-
-
-if __name__ == '__main__':
-    manager.run()
