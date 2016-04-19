83984a7b67f8c1322926a473879089aada0de7c9
Eric Rochester <erochest@virginia.edu>
2016-04-19 10:07:53 -0400

    Migrations.

--- frank/wsgi.py
+++ frank/wsgi.py
@@ -5,6 +5,8 @@
 
 from flask import Flask
 from flask.ext.heroku import Heroku
+from flask.ext.script import Manager
+from flask_migrate import Migrate, MigrateCommand
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
 
@@ -12,21 +14,22 @@ from sqlalchemy import text
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 heroku = Heroku(app)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-
-@app.before_request
-def before_request():
-    db.create_all()
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 
 @app.route('/')
-def main():
+def homepage():
     """The root page."""
     n = list(db.session.execute(text('SELECT RANDOM();'), {}))[0][0]
     return "It's alive!\n" + str(n) + "\n"
 
 
 if __name__ == '__main__':
-    main()
+    manager.run()
